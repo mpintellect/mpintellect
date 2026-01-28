a4f884afeba29597b2218c4ed7536f2bee975d89
mzprimer1 <m.s.marketing.dig@gmail.com>
2026-01-28 12:30:39 +0100

    Fix:edge runtime

--- next.config.ts
+++ next.config.ts
@@ -4,18 +4,11 @@ const nextConfig: NextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
-  // REMOVE THIS LINE: output: 'standalone',
   images: {
     unoptimized: true,
   },
   productionBrowserSourceMaps: false,
   turbopack: {},
-  webpack: (config, { dev }) => {
-    if (!dev) {
-      config.devtool = false;
-    }
-    return config;
-  },
 };
 
 export default nextConfig;

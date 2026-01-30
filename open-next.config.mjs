1e3ccbe98cdedaded25c1942b9c9bf5743183520
mzprimer1 <m.s.marketing.dig@gmail.com>
2026-01-30 01:39:57 +0100

    firebase removal

---
+++ open-next.config.mjs
@@ -0,0 +1,20 @@
+/** @type {import('@opennextjs/cloudflare').OpenNextConfig} */
+export default {
+  default: {
+    override: {
+      wrapper: "cloudflare-node",
+      converter: "edge",
+      incrementalCache: "dummy",
+      tagCache: "dummy",
+      queue: "dummy",
+    },
+  },
+  middleware: {
+    external: true,
+    override: {
+      wrapper: "cloudflare-edge",
+      converter: "edge",
+      proxyExternalRequest: "fetch",
+    },
+  },
+};
